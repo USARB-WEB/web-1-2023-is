8e28609e7b7db55e122bf007ffee8317ddb9d634
sergiuchilat <chilatsergiu@gmail.com>
2023-02-24 11:51:44 +0200

    Operators

---
+++ 3 - Operators/app.js
@@ -0,0 +1,33 @@
+let x = 2;
+let y = 0;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+x = 4;
+y = "a";
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
+console.log(5 / 2);
+console.log(5 % 2);
+
+
+let firstBoolean = true;
+let secondBoolean = false;
+
+console.log(true && true);
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
+
+console.log(4 > 2);
+console.log(4 <= 3);
+console.log(4 == "4");
+console.log(4 === "4");
